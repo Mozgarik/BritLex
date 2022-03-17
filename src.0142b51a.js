ce83d3676c41f4bfb44dbe658efce0a23de4bdd6
Mozgarik <91159717+Mozgarik@users.noreply.github.com>
2022-03-17 12:16:50 +0000

    Deploying to gh-pages from @ Mozgarik/BritLex@fc8218cc148e3dfcfcf90bbf027f8cf7c6750301 🚀

--- src.0142b51a.js
+++ src.0142b51a.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/BritLex/src.d689bdd0.js.map
+//# sourceMappingURL=/BritLex/src.0142b51a.js.map
